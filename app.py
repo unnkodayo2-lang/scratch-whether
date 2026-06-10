6a2170df9121cd1abb911365c9dad12e4857351c
unnkodayo2-lang <unnkodayo2@gmail.com>
2026-06-10 19:51:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import os
+import requests
 
-print("環境変数一覧")
-print(list(os.environ.keys()))
+key = os.environ["OPENWEATHER_KEY"]
 
-print("ある？")
-print("OPENWEATHER_KEY" in os.environ)
+print("APIキー取得成功")
+
+url = (
+    "https://api.openweathermap.org/data/2.5/weather"
+    f"?q=Tokyo&appid={key}&units=metric"
+)
+
+print("APIアクセス中")
+
+response = requests.get(url)
+
+print("ステータス")
+print(response.status_code)
+
+print(response.text)
