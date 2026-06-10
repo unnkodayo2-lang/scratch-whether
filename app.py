3aa709dedb4a68bbaa935d777496cebd7582db19
unnkodayo2-lang <unnkodayo2@gmail.com>
2026-06-10 20:12:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 import os
 import requests
+import scratchattach as scratch3
 
-key = os.environ["OPENWEATHER_KEY"]
-
-url = (
-    "https://api.openweathermap.org/data/2.5/weather"
-    f"?q=Tokyo&appid={key}&units=metric&lang=ja"
-)
-
-data = requests.get(url).json()
-
-print("気温:", data["main"]["temp"], "℃")
-print("天気:", data["weather"][0]["description"])
+print("ライブラリ読み込み成功")
