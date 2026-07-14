3ab8dc05b5f273012446e6c4ed37c0664e998737
unnkodayo2-lang <unnkodayo2@gmail.com>
2026-07-14 14:44:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,15 @@ session = scratch3.login(
     os.environ["SCRATCH_PASSWORD"]
 )
 
-# プロジェクト接続
+print("ログイン成功")
+
+# クラウド接続
 conn = session.connect_cloud("1352998508")
 
+print("クラウド接続成功")
+
 # OpenWeather APIキー
-key = os.environ["OPENWEATHER_KEY"]
+API_KEY = os.environ["OPENWEATHER_KEY"]
 
 
 @app.route("/")
@@ -26,16 +30,22 @@ def home():
 @app.route("/update")
 def update():
 
-    # 船橋の天気を取得
-    data = requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?lat=35.694&lon=139.983&appid={key}&units=metric",
-        timeout=10
-    ).json()
+    # 船橋市
+    url = (
+        f"https://api.openweathermap.org/data/2.5/weather"
+        f"?lat=35.694&lon=139.983"
+        f"&appid={API_KEY}"
+        f"&units=metric"
+        f"&lang=ja"
+    )
+
+    data = requests.get(url, timeout=10).json()
 
     # データ取得
     temp = round(data["main"]["temp"])
     feels_like = round(data["main"]["feels_like"])
     humidity = data["main"]["humidity"]
+    pressure = data["main"]["pressure"]
     wind = int(data["wind"]["speed"] * 100)
     clouds = data["clouds"]["all"]
     weather_id = data["weather"][0]["id"]
@@ -44,16 +54,19 @@ def update():
     conn.set_var("temp", temp)
     conn.set_var("feels_like", feels_like)
     conn.set_var("humidity", humidity)
+    conn.set_var("pressure", pressure)
     conn.set_var("wind", wind)
     conn.set_var("clouds", clouds)
     conn.set_var("weather_id", weather_id)
 
-    # 確認用JSON
+    print("更新成功")
+
     return {
         "status": "ok",
         "temp": temp,
         "feels_like": feels_like,
         "humidity": humidity,
+        "pressure": pressure,
         "wind": wind,
         "clouds": clouds,
         "weather_id": weather_id
